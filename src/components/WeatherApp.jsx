4869a529d6b0e4baa44f5c8ebeb46a1431ff508b
Diogo Xavier <diogodsxavier@gmail.com>
2025-02-28 14:54:17 -0300

    Update WeatherApp.jsx

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -9,6 +9,8 @@ function WeatherApp() {
      const [error, setError] = useState(null);
 
      const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+     console.log(API_KEY);
+     
 
      // Função para buscar dados do clima e da previsão
      const fetchWeather = useCallback(async () => {
